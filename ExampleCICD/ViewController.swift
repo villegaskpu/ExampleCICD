6ffc8bf63c45eb2f58ce6bc97be394ea2da83e4e
David Villegas Santana <davidvillegas14@gmail.com>
2023-08-18 10:55:45 -0600

    se agregam pruebas de chash y analiticos EVENTOS

--- ExampleCICD/ViewController.swift
+++ ExampleCICD/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import AppCenterCrashes
+import AppCenterAnalytics
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monthlyInvestmentsTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var retirementAgeTextField: UITextField!
+    @IBOutlet weak var interestRateTextField: UITextField!
+    @IBOutlet weak var savingsTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if Crashes.hasCrashedInLastSession {
+            let alert = UIAlertController(title: "Oops", message: "Sorry about that, an error occured.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "It's cool", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        Analytics.trackEvent("navigate_to_calculator")
+    }
 
-
+    @IBAction func calculateButton_TouchUpInside(_ sender: Any) {
+//        Crashes.generateTestCrash()
+        Analytics.trackEvent("calculate_event_amount")
+    }
 }
 
