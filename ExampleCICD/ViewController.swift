65ee21980c87cbbb6dcd7b91af234e1c68e3a6b8
David Villegas Santana <davidvillegas14@gmail.com>
2023-08-18 11:07:35 -0600

    Add feature 1

--- ExampleCICD/ViewController.swift
+++ ExampleCICD/ViewController.swift
@@ -35,7 +35,18 @@ class ViewController: UIViewController {
 
     @IBAction func calculateButton_TouchUpInside(_ sender: Any) {
 //        Crashes.generateTestCrash()
-        Analytics.trackEvent("calculate_event_amount")
+        let current_age : Int? = Int(ageTextField.text!)
+        let planned_retirement_age : Int? = Int(retirementAgeTextField.text!)
+        let monthly_investment : Float? = Float(monthlyInvestmentsTextField.text!)
+        let current_savings : Float? = Float(savingsTextField.text!)
+        let interest_rate : Float? = Float(interestRateTextField.text!)
+        
+        resultLabel.text = "If you save $\(monthly_investment!) every month for \(planned_retirement_age! - current_age!) years, and invest that money plus your current investment of $\(current_savings!) at a \(interest_rate!)% anual interest rate, you will have $X by the time you are \(planned_retirement_age!)"
+        
+        let properties = ["current_age": String(current_age!),
+                          "planned_retirement_age": String(planned_retirement_age!)]
+        
+        Analytics.trackEvent("calculate_retirement_amount", withProperties: properties)
     }
 }
 
